e8e6fd43f1e4d8d2f375d6f8349f7ec487c40dfd
Mike Byrne <mike@area17.com>
2023-02-13 20:06:49 +0000

    initial commit 🎉

---
+++ README.md
@@ -0,0 +1,102 @@
+# parse-numeric-date
+
+Parse numeric based date inputs.
+
+## Mission Statement
+
+Date entry is hard for users. Date pickers, including the native one, aren't always the best for accessibility. 
+
+In his article ["Maybe You Don't Need a Date Picker"](https://adrianroselli.com/2019/07/maybe-you-dont-need-a-date-picker.html#Messaging), Adrian Roselli proposes using a text field which users enter a date into, some client side does some parsing and then some feedback is presented to the user.
+
+Its fairly straight forward to parse a known date format and convert to ISO format. But, what if users don't enter the date in your known format?
+
+Well, that is what `parse-numeric-date` attempts to solve.
+
+## Demo
+
+[https://area17.github.io/parse-numeric-date/](https://area17.github.io/parse-numeric-date/)
+
+## Usage
+
+Firstly, install via npm:
+
+```
+$ npm install @area17/parse-numeric-date
+```
+
+Import into your JavaScript by:
+
+```
+import parseNumericDate from '@area17/parse-numeric-date'
+```
+
+And then in your JavaScript:
+
+```
+let isoString = parseNumericDate('29/12/25'); // 2025-12-29
+```
+
+If you want to have manual control over the locale:
+
+```
+let isoString = parseNumericDate('29/12/25', {
+    locale: 'en-GB' // will favor d/m/y over m/d/y
+}); // 2025-12-29
+```
+
+## Examples of what `parse-numeric-date` can parse
+
+* 2022-02-10
+* 2022.02.10
+* 2022/02/10
+* 2022-2-10
+* 2022-2-2
+* 10 02 2022
+* 1 12 2022
+* 122922
+* 090322
+* 9322
+* 29122022
+* 010122
+* 12/29/2022
+* 12/29/22
+* 01-01-2022
+* 01-01-22
+* 1-1-22
+* 1-1-2022
+
+## Notes
+
+This `parse-numeric-date` works with a big assumption:
+
+*Dates closer to today are better than dates years into the future*
+
+Date pickers for hotels, restaurants or events tend to be for things happening this year or next year. Dates of birth tend to happen from now to 100 years in the past. Retirement ages, mortgage completions tend to happen from now to 100 years in the future. 
+
+If the year difference is larger than 100, its probably useless.
+eg: a 29 in the string appear as 2922 rather than 29th of a month, 2022
+
+## Limitations
+
+`parse-numeric-date` can't take natural input such as "December 12, 2022" - it is purely for parsing numeric inputs. Another assumption here is that such date entry, although natural in speech, isn't so natural to write or type - especially if the keyboard has been switched to a numeric keyboard on the users device.
+
+If you want such parsing, I suggest you look at:
+* [`parse-numeric-date`](https://github.com/kensnyder/any-date-parser)
+* [`parse-dob`](https://github.com/HenrikJoreteg/parse-dob)
+
+Both are excellent, but can't parse numeric dates quite as fully as `parse-numeric-date`.
+
+## Comparison with other libraries
+
+You can see a comparison between [`parse-numeric-date`](https://github.com/kensnyder/any-date-parser) and `anyDateParser` and [`parse-dob`](https://github.com/HenrikJoreteg/parse-dob) over on Codepen: [Date parse testing](https://codepen.io/13twelve/pen/rNrRoLB?editors=0010)
+
+`parse-dob` by [@HenrikJoreteg](https://github.com/HenrikJoreteg) was a great inspiration, we think that forking `parse-dob` and removing the previous date restriction would be very interesting.
+
+## Future development
+
+* Add tests (and not just use [https://area17.github.io/parse-numeric-date/](https://area17.github.io/parse-numeric-date/) for testing)
+* Add support for other international date formats, such as Chinese and Persian dates.
+
+## Code of Conduct
+
+AREA 17 is dedicated to building a welcoming, diverse, safe community. We expect everyone participating in the AREA 17 community to abide by our [Code of Conduct](CODE_OF_CONDUCT.md). Please read it. Please follow it.
---
+++ src/index.js
@@ -0,0 +1,320 @@
+/*
+  TODO:
+
+  Persian Dates:
+
+  ` ١٨‏/١‏/١٩`
+  ` ۱۳۸۹/۵/۱۶`
+
+  Chinese Dates:
+
+  1924年9月25日
+  25‏/9‏/1924.
+*/
+
+
+let locale = new Intl.NumberFormat().resolvedOptions().locale;
+const twoDigitCutoffYear = 60;
+const today = new Date().setHours(0, 0, 0, 0);
+const thisYear = new Date(today).getFullYear();
+const yearInMs = 365.25 * 24 * 60 * 60 * 1000;
+const twoYearsInMs = yearInMs * 2;
+const hundredYearsInMs = yearInMs * 100;
+const getLocaleOrder = () => {
+  const dateStr = new Date(2000, 11, 13).toLocaleDateString(locale);
+  const parts = [
+    { name: 'd', index: dateStr.indexOf('13') },
+    { name: 'm', index: dateStr.indexOf('12') },
+    { name: 'y', index: dateStr.indexOf('2000') },
+  ].sort((a, b) => a.index - b.index);
+  return parts.map(item => item.name).join('-');
+};
+
+let localeOrder = getLocaleOrder();
+
+// Month in JavaScript is 0-indexed (January is 0, February is 1, etc),
+// but by using 0 as the day it will give us the last day of the prior
+// month. So passing in 1 as the month number will return the last day
+// of January, not February
+const daysInMonth = (m, y) => {
+  return new Date(y, m, 0).getDate();
+};
+
+const make4DigitYear = (y) => {
+  if (y.length === 2) {
+    return parseInt(y) > twoDigitCutoffYear ? `19${y}` : `20${y}`;
+  }
+  return y;
+}
+
+const generateDate = (y, m, d) => {
+  // if year is 2 digits, guess 19XX or 20XX
+  y = make4DigitYear(y);
+
+  // convert to numbers
+  y = parseInt(y, 10);
+  m = parseInt(m, 10);
+  d = parseInt(d, 10);
+
+  // stop months being over 12
+  m = m > 12 ? null : m;
+
+  // stop day being over how many days are in the month
+  d = d > daysInMonth(m, y) ? null : d;
+
+  // pad month and day
+  m = m < 10 ? `0${m}` : m;
+  d = d < 10 ? `0${d}` : d;
+
+  // test date
+  let t = new Date(`${y}-${m}-${d}T00:00:00.000+00:00`);
+
+  if (t instanceof Date && !isNaN(t)) {
+    return t;
+  } else {
+    return null;
+  }
+};
+
+const getMostProbableDate = (dates) => {
+  // make date strs, set order score
+  // filter out nulls
+  // order by score
+  dates = dates
+    .map(obj => {
+      // make date
+      obj.d = generateDate(obj.y, obj.m, obj.d);
+      // make year a number
+      obj.y = parseInt(make4DigitYear(obj.y), 10);
+      // find how far away from this year the date is
+      //obj.diff = Math.abs(new Date(today).getFullYear() - obj.y);
+      obj.diff = Math.abs(today - obj.d);
+      // if the date is in locale order, give it a higher score
+      obj.score = localeOrder === obj.order ? 2 : 1;
+      //
+      // Assumption: dates closer to today are better
+      // Why: date pickers for hotels, restaurants, events tend to
+      // happen this year or next year. Dates of birth tend to happen
+      // from now to 100 years in the past. Retirement ages, mortgage
+      // completions tend to happen from now to 100 years in the future
+      //
+      // if the year difference is larger than 100, its probably useless
+      // eg: a 29 made it 2922 rather than 29th of a month, 2022
+      if (obj.diff > hundredYearsInMs) {
+        obj.score--;
+      }
+      // year is really quite close to this year, probably more likely
+      if (obj.diff < twoYearsInMs) {
+        obj.score++;
+      }
+
+      return obj;
+    })
+    // remove any null dates from invalid dates being made
+    .filter(obj => obj.d !== null)
+    // sort by score, high to low
+    .sort((a, b) => {
+      return b.score - a.score;
+    });
+
+  // whats our highest score?
+  const highestScore = dates[0].score;
+
+  // remove anything that hasn't got the lowest score
+  // get just the dates
+  // order by closest to today
+  dates = dates
+    // remove anything that doesn't match our high score
+    .filter(obj => obj.score === highestScore)
+    // sort by year distance, low to high
+    .sort((a, b) => {
+      return a.diff - b.diff;
+    })
+    .map(obj => obj.d);
+
+  // return first date (highest score, lowest year difference)
+  if (dates.length) {
+    return dates[0];
+  }
+  return null;
+};
+
+const parseNumericDate = (dateStr, options) => {
+  if (options.locale) {
+    locale = options.locale;
+    localeOrder = getLocaleOrder();
+  }
+
+  dateStr = dateStr.trim();
+  let dates = [];
+
+  // input probably = ISO YYYY-MM-DD or YYYY-M-D
+  let matches = [
+    ...dateStr.matchAll(/([0-9]{4})[^0-9]{1}([0-9]{1,2})[^0-9]{1}([0-9]{1,2})/g)
+  ];
+  if (matches.length > 0) {
+    return generateDate(matches[0][1], matches[0][2], matches[0][3]);
+  }
+
+  // input doesn't follow ISO YYYY-MM-DD
+  // lets check for DD-MM-YYYY (or MM-DD-YYYY)
+  matches = [
+    ...dateStr.matchAll(/([0-9]{1,2})[^0-9]{1}([0-9]{2})[^0-9]{1}([0-9]{4})/g)
+  ];
+  if (matches.length > 0) {
+    dates.push({
+      y: matches[0][3],
+      m: matches[0][2],
+      d: matches[0][1],
+      order: 'd-m-y'
+    });
+
+    dates.push({
+      y: matches[0][3],
+      m: matches[0][1],
+      d: matches[0][2],
+      order: 'm-d-y'
+    });
+    //
+    return getMostProbableDate(dates, dateStr);
+  }
+
+  // lets check for DD-MM-YY (or MM-DD-YY), YY-MM-DD
+  // and D-M-YY, M-DD-YY
+  matches = [
+    ...dateStr.matchAll(
+      /([0-9]{1,2})[^0-9]{1}([0-9]{1,2})[^0-9]{1}([0-9]{1,2})/g
+    )
+  ];
+  if (matches.length > 0) {
+    dates.push({
+      y: matches[0][3],
+      m: matches[0][2],
+      d: matches[0][1],
+      order: 'd-m-y'
+    });
+
+    dates.push({
+      y: matches[0][3],
+      m: matches[0][1],
+      d: matches[0][2],
+      order: 'm-d-y'
+    });
+
+    dates.push({
+      y: matches[0][1],
+      m: matches[0][2],
+      d: matches[0][3],
+      order: 'y-m-d'
+    });
+    //
+    return getMostProbableDate(dates, dateStr);
+  }
+
+  // input has no spaces between dates
+  // very much guessing now
+
+  // could be YYYYMMDD, DDMMYYYY, MMDDYYYY
+  if (dateStr.length === 8) {
+    dates.push({
+      y: dateStr.substring(4, 8),
+      m: dateStr.substring(2, 4),
+      d: dateStr.substring(0, 2),
+      order: 'd-m-y'
+    });
+
+    dates.push({
+      y: dateStr.substring(4, 8),
+      m: dateStr.substring(0, 2),
+      d: dateStr.substring(2, 4),
+      order: 'm-d-y'
+    });
+
+    dates.push({
+      y: dateStr.substring(0, 4),
+      m: dateStr.substring(4, 6),
+      d: dateStr.substring(6, 8),
+      order: 'y-m-d'
+    });
+    //
+    return getMostProbableDate(dates, dateStr);
+  }
+
+  // or could be YYMMDD, DDMMYY, MMDDYY
+  // or worse, could be YYYYMD, DMYYYY, MDYYYY
+  if (dateStr.length === 6) {
+    dates.push({
+      y: dateStr.substring(4, 6),
+      m: dateStr.substring(2, 4),
+      d: dateStr.substring(0, 2),
+      order: 'd-m-y'
+    });
+
+    dates.push({
+      y: dateStr.substring(4, 6),
+      m: dateStr.substring(0, 2),
+      d: dateStr.substring(2, 4),
+      order: 'm-d-y'
+    });
+
+    dates.push({
+      y: dateStr.substring(0, 2),
+      m: dateStr.substring(2, 4),
+      d: dateStr.substring(4, 6),
+      order: 'y-m-d'
+    });
+
+    /**/
+    dates.push({
+      y: dateStr.substring(2, 6),
+      m: dateStr.substring(1, 2),
+      d: dateStr.substring(0, 1),
+      order: 'd-m-y'
+    });
+
+    dates.push({
+      y: dateStr.substring(2, 6),
+      m: dateStr.substring(0, 1),
+      d: dateStr.substring(1, 2),
+      order: 'm-d-y'
+    });
+
+    dates.push({
+      y: dateStr.substring(0, 4),
+      m: dateStr.substring(4, 5),
+      d: dateStr.substring(5, 6),
+      order: 'y-m-d'
+    });
+
+    //
+    return getMostProbableDate(dates, dateStr);
+  }
+
+  // could be YYMD, DMYY, MDYY
+  if (dateStr.length === 4) {
+    dates.push({
+      y: dateStr.substring(2, 4),
+      m: dateStr.substring(1, 2),
+      d: dateStr.substring(0, 1),
+      order: 'd-m-y'
+    });
+
+    dates.push({
+      y: dateStr.substring(2, 4),
+      m: dateStr.substring(0, 1),
+      d: dateStr.substring(1, 2),
+      order: 'm-d-y'
+    });
+
+    dates.push({
+      y: dateStr.substring(0, 2),
+      m: dateStr.substring(2, 3),
+      d: dateStr.substring(3, 4),
+      order: 'y-m-d'
+    });
+    //
+    return getMostProbableDate(dates, dateStr);
+  }
+};
+
+export default parseNumericDate;
